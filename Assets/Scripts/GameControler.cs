8cd8f41e337b179959b6c746278409d26605ec8b
DESKTOP-1F2LFR5\SDiego <sev17238@uvg.edu.gt>
2018-04-11 20:18:12 -0600

    Hoja de trabajo de Flappy bird infinito terminada. Aun asi los extras estan pendientes! :D

---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour {
+    public bool gameover = false;
+    public int score = 0;
+    public static GameControler instance;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
